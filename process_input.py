ffe9becb13601aa2d62cf9ac4366e080ae1d5e03
Dan Bravender <dan.bravender@gmail.com>
2015-04-29 01:45:21 +0900

    3x faster than the last attempt at multiprocessing

--- process_input.py
+++ process_input.py
@@ -1,10 +1,10 @@
 from multiprocessing import Pool, cpu_count
-from collections import defaultdict, namedtuple
+from collections import defaultdict
 import ujson as json
 
-POOL_SIZE = cpu_count()
-
+POOL_SIZE = cpu_count() / 2
 entries = defaultdict(lambda: [])
+
 
 def process_site(site_id):
     """Returns the string used in reports:
@@ -16,12 +16,12 @@ def process_site(site_id):
     seen_messages = set()
     sent_messages = 0
     sent_emails = 0
-    for timestamp, _id, _from, online in sorted(entries[site_id]):
+    for timestamp, _id, is_message, _from, online in sorted(entries[site_id]):
         if _id in seen_messages:
             continue
         seen_messages.add(_id)
-        if entry.is_message:
-            seen_visitors.add(entry.from_id)
+        if is_message:
+            seen_visitors.add(_from)
             if online_operators:
                 sent_messages += 1
             else:
@@ -29,22 +29,16 @@ def process_site(site_id):
         else:
             if online:
                 online_operators.add(_from)
-            elif entry.from_id in online_operators:
+            elif _from in online_operators:
                 online_operators.remove(_from)
-            seen_operators.add(entry.from_id)
-    # The sort needs to be alphanumeric so we return the id as a string
+            seen_operators.add(_from)
     return ('{},messages={},emails={},operators={},visitors={}'.format(
                 site_id, sent_messages, sent_emails, len(seen_operators),
                 len(seen_visitors)))
 
 
-# assert parse_entry('{"id":"7f360d94-ecae-4d8e-a26a-e11cea7bde2b","type":"message","from":"visitor5","site_id"'
-#                       ':"1","timestamp":4301,"data":{"message":"sorting than been"}}') == (4301, Entry(is_message=True, online=False, unique_id='7f360d94-ecae-4d8e-a26a-e11cea7bde2b', from_id='visitor5'))
-# assert parse_entry('{"id":"f0d6ebe9-4830-415b-a826-a6ef47ebb649","type":"status","from":"operator1","site_id":"1","timestamp":5630,"data":{"status":"online"}}') == (5630, Entry(is_message=False, online=True, unique_id='f0d6ebe9-4830-415b-a826-a6ef47ebb649', from_id='operator1'))
-
 if __name__ == '__main__':
-    p = Pool(POOL_SIZE)
-    site_ids = []
+    site_ids = set()
     for line in open('big_input').readlines():
         entry = json.loads(line)
         site_id = str(entry['site_id'])
@@ -53,9 +47,12 @@ if __name__ == '__main__':
             entry['id'],
             entry['type'] == 'message',
             entry['from'],
-            int(entry.get('data', {}).get('status', '') == 'online'),
+            entry.get('data', {}).get('status', '') == 'online',
         ))
-        site_ids.append(site_id)
+        site_ids.add(site_id)
 
-    for response in p.imap(process_site, sorted(site_ids)):
+    # fork here so the child processes each have a copy of the entries
+    # dictionary
+    p = Pool(POOL_SIZE)
+    for response in p.map(process_site, sorted(site_ids)):
         print response
