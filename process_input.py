5b2a3387f39effed3b4244392ecacd4f6c7652ee
Dan Bravender <dan.bravender@gmail.com>
2015-04-29 01:24:47 +0900

    A failed attempt to use a shared object in global memory

--- process_input.py
+++ process_input.py
@@ -2,34 +2,24 @@ from multiprocessing import Pool, cpu_count
 from collections import defaultdict, namedtuple
 import ujson as json
 
-PoolSize = cpu_count() / 2
+POOL_SIZE = cpu_count()
 
+entries = defaultdict(lambda: [])
 
-# Using namedtuples will help save memory when dealing with many objects
-Entry = namedtuple('Entry', 'unique_id, from_id, online, is_message')
-TimestampEntry = namedtuple('TimestampEntry', 'timestamp, entry')
-SiteTimestampEntry = namedtuple('SiteTimestampEntry',
-                                'site_id, timestampentry')
-
-
-def process_site(id_timestamp_entries):
-    """Returns a tupe that looks like this:
-       ('124', 'messages=2,emails=1,operators=4,visitors=1')
-
-    Expects that entries will be a list of (timestamp, Entry) tuples
+def process_site(site_id):
+    """Returns the string used in reports:
+       '124,messages=2,emails=1,operators=4,visitors=1'
     """
-    site_id, timestamp_entries = id_timestamp_entries
     online_operators = set()
     seen_operators = set()
     seen_visitors = set()
     seen_messages = set()
     sent_messages = 0
     sent_emails = 0
-    for timestamp_entry in sorted(timestamp_entries):
-        entry = timestamp_entry.entry
-        if entry.unique_id in seen_messages:
+    for timestamp, _id, _from, online in sorted(entries[site_id]):
+        if _id in seen_messages:
             continue
-        seen_messages.add(entry.unique_id)
+        seen_messages.add(_id)
         if entry.is_message:
             seen_visitors.add(entry.from_id)
             if online_operators:
@@ -37,29 +27,15 @@ def process_site(id_timestamp_entries):
             else:
                 sent_emails += 1
         else:
-            if entry.online:
-                online_operators.add(entry.from_id)
+            if online:
+                online_operators.add(_from)
             elif entry.from_id in online_operators:
-                online_operators.remove(entry.from_id)
+                online_operators.remove(_from)
             seen_operators.add(entry.from_id)
     # The sort needs to be alphanumeric so we return the id as a string
-    return (str(site_id),
-            'messages={},emails={},operators={},visitors={}'.format(
-                sent_messages, sent_emails, len(seen_operators),
+    return ('{},messages={},emails={},operators={},visitors={}'.format(
+                site_id, sent_messages, sent_emails, len(seen_operators),
                 len(seen_visitors)))
-
-
-def parse_entry(raw_entry):
-    entry = json.loads(raw_entry)
-    return SiteTimestampEntry(
-        site_id=entry['site_id'],
-        timestampentry=TimestampEntry(
-            timestamp=entry['timestamp'],
-            entry=Entry(
-                is_message=(entry['type'] == 'message'),
-                online=entry.get('data', {}).get('status', '') == 'online',
-                unique_id=entry['id'],
-                from_id=entry['from'])))
 
 
 # assert parse_entry('{"id":"7f360d94-ecae-4d8e-a26a-e11cea7bde2b","type":"message","from":"visitor5","site_id"'
@@ -67,11 +43,19 @@ def parse_entry(raw_entry):
 # assert parse_entry('{"id":"f0d6ebe9-4830-415b-a826-a6ef47ebb649","type":"status","from":"operator1","site_id":"1","timestamp":5630,"data":{"status":"online"}}') == (5630, Entry(is_message=False, online=True, unique_id='f0d6ebe9-4830-415b-a826-a6ef47ebb649', from_id='operator1'))
 
 if __name__ == '__main__':
-    p = Pool(PoolSize)
-    site_data = defaultdict(lambda: [])
+    p = Pool(POOL_SIZE)
+    site_ids = []
     for line in open('big_input').readlines():
-        sitetimestampentry = parse_entry(line)
-        site_data[sitetimestampentry.site_id].append(sitetimestampentry.timestampentry)
+        entry = json.loads(line)
+        site_id = str(entry['site_id'])
+        entries[site_id].append((
+            entry['timestamp'],
+            entry['id'],
+            entry['type'] == 'message',
+            entry['from'],
+            int(entry.get('data', {}).get('status', '') == 'online'),
+        ))
+        site_ids.append(site_id)
 
-    for site_id, results in sorted(p.imap_unordered(process_site, site_data.items())):
-        print '{},{}'.format(site_id, results)
+    for response in p.imap(process_site, sorted(site_ids)):
+        print response
