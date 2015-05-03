0fe832c0efefb5dce55f6c8b36f9d03846a2c9ff
Dan Bravender <dan.bravender@gmail.com>
2015-05-04 01:37:10 +0900

    Trying sqlite3

--- process_input.py
+++ process_input.py
@@ -1,31 +1,32 @@
 from multiprocessing import Pool, cpu_count
-from glob import glob
-import numpy as np
-import os
-from shutil import rmtree
+import sqlite3
 import sys
+from threading import local
 import ujson as json
 
 POOL_SIZE = cpu_count()
+threadlocal = local()
+threadlocal.read_connection = None
 
 
-def process_site(work_path):
+def process_site(site_id):
     """Returns the string used in reports:
        '124,messages=2,emails=1,operators=4,visitors=1'
     """
-    site_id = work_path[5:]
     online_operators = set()
     seen_operators = set()
     seen_visitors = set()
-    seen_messages = set()
     sent_messages = 0
     sent_emails = 0
-    entries = np.genfromtxt(work_path, delimiter=',', dtype=None)
-    entries.sort()
+    if not threadlocal.read_connection:
+        threadlocal.read_connection = sqlite3.connect('work.db')
+    cursor = threadlocal.read_connection.cursor()
+    entries = cursor.execute(
+        'SELECT DISTINCT _timestamp, _id, is_message, _from, online '
+        'FROM entries '
+        'WHERE site_id = ? '
+        'ORDER BY _timestamp', (site_id,))
     for timestamp, _id, is_message, _from, online in entries:
-        if _id in seen_messages:
-            continue
-        seen_messages.add(_id)
         if is_message:
             seen_visitors.add(_from)
             if online_operators:
@@ -44,21 +45,32 @@ def process_site(work_path):
 
 
 if __name__ == '__main__':
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    rmtree('work')
-    os.mkdir('work')
-    pool = Pool(POOL_SIZE)
+    connection = sqlite3.connect('work.db')
+    cursor = connection.cursor()
+    cursor.execute('DROP TABLE IF EXISTS entries')
+    cursor.execute('CREATE TABLE entries (site_id TEXT, _timestamp INTEGER, '
+                   '_id INTEGER, is_message INTEGER, _from TEXT, '
+                   'online INTEGER)')
+    cursor.execute('CREATE INDEX site_id_timestamp_idx '
+                   'ON entries (site_id, _timestamp)')
     for line in sys.stdin:
         entry = json.loads(line)
-        is_message = entry['type'] == 'message' and 1 or 0
-        csv_line = '{},{},{},{},{}\n'.format(
+        cursor.execute('INSERT INTO entries VALUES (?,?,?,?,?,?)', (
+            entry['site_id'],
             entry['timestamp'],
             entry['id'],
-            is_message,
+            entry['type'] == 'message' and 1 or 0,
             entry['from'],
             entry.get('data', {}).get('status', '') == 'online' and 1 or 0
-        )
-        open('work/{}'.format(entry['site_id']), 'a').write(csv_line)
+        ))
 
-    for response in pool.imap(process_site, sorted(glob('work/*'))):
+    connection.commit()
+    connection.close()
+    read_connection = sqlite3.connect('work.db')
+    pool = Pool(POOL_SIZE)
+    cursor = read_connection.cursor()
+    site_ids = cursor.execute('SELECT DISTINCT site_id FROM entries '
+                              'ORDER BY site_id')
+    for response in pool.imap(process_site,
+                              (s[0] for s in site_ids.fetchall())):
         print response
